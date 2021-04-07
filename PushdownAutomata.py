b108d029c619585b126ff7d9c2ac68073374e4a9
Matthew Krohn <matthew.d.krohn@gmail.com>
2021-04-06 19:56:06 -0600

    Think I have it basically working?

--- PushdownAutomata.py
+++ PushdownAutomata.py
@@ -22,8 +22,9 @@ def valid_transition(transition: dict, valid_states: list[str], alphabet: str) -
                             return False
 
                 elif info not in valid_states:
-                        print(f"Invalid 'else' state: {info}")
-                        return False
+                    print(f"Invalid 'else' state: {info}")
+                    return False
+
     return True
 
 
@@ -31,12 +32,15 @@ class PushdownAutomata:
     def __init__(self, definition: dict[str, Any]):
         try:
             self.alphabet = definition["alphabet"]
-            self.states = definition["states"]
+            self.states = list(set(definition["states"]))
+            if len(self.states) != len(definition["states"]):
+                raise Exception("Duplicate states found")
             self.initial: str = definition["initial"]
             self.end = list(definition["end"])
             self.final = list(definition["final"])
             self.stack: list = definition.get("stack", [])
             self.transitions: dict[str, Any] = definition.get("transitions", {})
+            self.current = self.initial
             if len(self.transitions) == 0:
                 raise Exception("'transitions' must not be empty")
 
@@ -46,14 +50,49 @@ class PushdownAutomata:
 
                 if not valid_transition(value, self.states, self.alphabet):
                     exit(1)
-
-            print(self)
-
         except KeyError as e:
             print(f"Could not find required value {e}")
 
     def run(self, word: str):
-        pass
+        self.current = self.initial
+        print(f"Testing word: {word}")
+        self.word = word
+        while len(self.word) > 0 and self.current not in self.end:
+            print(f"Transitioning from {self.current} via char '{self.word[0]}'")
+            self.transition()
+
+        if self.current in self.final:
+            print(f"'{word}' is accepted by given automata\n")
+        else:
+            print(
+                f"""'{word}' is not accepted
+\tCurrent state: {self.current}
+\tRemaining text: {self.word}\n"""
+            )
+
+    def transition(self):
+        char = self.word[0]
+        transition = self.transitions.get(self.current)
+        if transition is None:
+            self.current = "error"
+            return
+
+        transition_info = transition.get(char)
+        if type(transition_info) is str:
+            self.current = transition_info
+        elif type(transition_info) is dict:
+            for entry, data in transition_info.items():
+                if len(self.stack) > 0 and self.stack[-1] == entry:
+                    self.current = data
+                    self.stack = self.stack[: -data.get("pop", 0)]
+                    self.stack.extend(data.get("push", []))
+
+            if transition_info.get("else") is None:
+                self.current = "error"
+            else:
+                self.current = transition_info.get("else")
+
+        self.word = self.word[1:]
 
     def __str__(self):
         return f"""Alphabet: {self.alphabet}
@@ -62,4 +101,5 @@ Initial: {self.initial}
 End: {self.end}
 Final: {self.final}
 Stack: {self.stack}
+Current: {self.current}
 Transitions: {self.transitions}"""
